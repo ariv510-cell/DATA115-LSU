15deccc7a257d17458f900e723b241ad2a3c34b7
ariv510-cell <ariv510@lasierra.edu>
2026-05-21 13:04:32 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,11 @@
-// Data115 Full Extension
-//% color="#1E90FF" icon="\uf1b2" block="Data115"
-namespace Data115 {
-    //% block="hello Data115"
-    export function hello(): void {
-        basic.showString("D115")
-    }
+basic.forever(function () {
 
-    //% block="map value $value from low $fromLow high $fromHigh to low $toLow high $toHigh"
-    export function mapValue(value: number, fromLow: number, fromHigh: number, toLow: number, toHigh: number): number {
-        return Math.idiv((value - fromLow) * (toHigh - toLow), (fromHigh - fromLow)) + toLow
-    }
+    Data115OLED.init()
 
-    //% block="clamp value $value min $min max $max"
-    export function clamp(value: number, min: number, max: number): number {
-        if (value < min) return min
-        if (value > max) return max
-        return value
-    }
-}
+    Data115OLED.smiley()
+    basic.pause(1000)
+
+    Data115OLED.blink()
+    basic.pause(300)
+
+})
